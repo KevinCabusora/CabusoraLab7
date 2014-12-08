aa4bb30e7f500f0402fe39345c3c1b70deb8e191
Kevin Cabusora <c16kevin.cabusora@usafa.edu>
2014-12-07 22:47:34 -0700

    updated

--- lab7.c
+++ lab7.c
@@ -4,14 +4,14 @@
  *  Created on: Dec 5, 2014
  *      Author: C16Kevin.Cabusora
  */
+#include "lab7.h"
 
 #include <msp430g2553.h>
-#include "lab7.h"
+
 
 
 
 void left(){
-	WDTCTL = WDTPW + WDTHOLD;
 	ADC10CTL0 = ADC10SHT_3 + ADC10ON + ADC10IE;
 	ADC10CTL1 = INCH_1;
 	ADC10AE0 |= BIT1;
@@ -20,27 +20,25 @@ void left(){
 
 		ADC10CTL0 |= ENC + ADC10SC;
 		__bis_SR_register(CPUOFF + GIE);
-		if (ADC10MEM < 0x1FF){
+		if (ADC10MEM < 0x230){
 			P1OUT &= ~BIT0;
-		}
-		else{
+
+		else
 			P1OUT |= BIT0;
-		}
 }
 
 void middle(){
-{
-  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+
+             // Stop WDT
   ADC10CTL0 = ADC10SHT_3 + ADC10ON + ADC10IE; // ADC10ON, interrupt enabled
   ADC10CTL1 = INCH_2;                       // input A4
   ADC10AE0 |= BIT2;                         // PA.1 ADC option select
   ADC10CTL1 |= ADC10SSEL1|ADC10SSEL0;                // Select SMCLK
   P1DIR |= BIT0|BIT6;                            // Set P1.0 to output direction
 
-
     ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
     __bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
-    if (ADC10MEM < 0x1FF){
+    if (ADC10MEM < 0x230){
       P1OUT &= ~(BIT0|BIT6);                       // Clear P1.0 LED off
     }
      else{
@@ -49,7 +47,7 @@ void middle(){
 }
 
 
-void right(){
+void right()
 {
   WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
   ADC10CTL0 = ADC10SHT_3 + ADC10ON + ADC10IE; // ADC10ON, interrupt enabled
@@ -60,7 +58,7 @@ void right(){
 
     ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
     __bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
-    if (ADC10MEM < 0x1FF){
+    if (ADC10MEM < 0x230){
       P1OUT &= ~BIT6;                       // Clear P1.0 LED off
     }
      else{
