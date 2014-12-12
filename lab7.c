692e62ad12a7764e261d35836dd1c96d1b761f2a
KevinCabusora <c16kevin.cabusora@usafa.edu>
2014-12-12 13:54:46 -0700

    Update Lab7.C

--- lab7.c
+++ lab7.c
@@ -10,59 +10,51 @@
 
 void left()
 {
-	ADC10CTL0 = ADC10SHT_3 + ADC10ON + ADC10IE;
-	ADC10CTL1 = INCH_1;
-	ADC10AE0 |= BIT1;
+	ADC10CTL0 = ADC10SHT_3 + ADC10ON + ADC10IE; //ADC10ON,interrupt enabled
+	ADC10CTL1 = INCH_3;				// input A3
+	ADC10AE0 |= BIT3;				
 	ADC10CTL1 |= ADC10SSEL1|ADC10SSEL0;
 	P1DIR |= BIT0;
 
 		ADC10CTL0 |= ENC + ADC10SC;
 		__bis_SR_register(CPUOFF + GIE);
-		if (ADC10MEM < 0x230)
+		if (ADC10MEM < 0x1FF)
 			P1OUT &= ~BIT0;
-
 		else
 			P1OUT |= BIT0;
-
-
 }
 
 void middle()
 {
-             // Stop WDT
-  ADC10CTL0 = ADC10SHT_3 + ADC10ON + ADC10IE; // ADC10ON, interrupt enabled
-  ADC10CTL1 = INCH_2;                       // input A4
-  ADC10AE0 |= BIT2;                         // PA.1 ADC option select
-  ADC10CTL1 |= ADC10SSEL1|ADC10SSEL0;                // Select SMCLK
-  P1DIR |= BIT0|BIT6;                            // Set P1.0 to output direction
+	ADC10CTL0 = ADC10SHT_3 + ADC10ON + ADC10IE; // ADC10ON, interrupt enabled
+  	ADC10CTL1 = INCH_4;                       // input A5
+	ADC10AE0 |= BIT4;                         // PA.1 ADC option select
+  	ADC10CTL1 |= ADC10SSEL1|ADC10SSEL0;                // Select SMCLK
+  	P1DIR |= BIT0|BIT6;                            // Set P1.0, P1.6 to output direction
 
-    ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
-    __bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
-    if (ADC10MEM < 0x230){
-      P1OUT &= ~(BIT0|BIT6);                       // Clear P1.0 LED off
-    }
-     else{
-      P1OUT |= BIT0|BIT6;                        // Set P1.0 LED on
-	}
+		ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
+		__bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
+		if (ADC10MEM < 0x1FF)
+      			P1OUT &= ~(BIT0|BIT6);                       // Clear P1.0, P1.6 LED off
+     		else
+      			P1OUT |= BIT0|BIT6;                        // Set P1.0 LED on
 }
 
 
 void right()
 {
-  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
-  ADC10CTL0 = ADC10SHT_3 + ADC10ON + ADC10IE; // ADC10ON, interrupt enabled
-  ADC10CTL1 = INCH_3;                       // input A4
-  ADC10AE0 |= BIT3;                         // PA.1 ADC option select
-  ADC10CTL1 |= ADC10SSEL1|ADC10SSEL0;                // Select SMCLK
-  P1DIR |= BIT6;                            // Set P1.0 to output direction
+  	ADC10CTL0 = ADC10SHT_3 + ADC10ON + ADC10IE; // ADC10ON, interrupt enabled
+  	ADC10CTL1 = INCH_5;                       // input A5
+	ADC10AE0 |= BIT5;                         // PA.1 ADC option select
+  	ADC10CTL1 |= ADC10SSEL1|ADC10SSEL0;                // Select SMCLK
+  	P1DIR |= BIT6;                            // Set P1.0 to output direction
 
-    ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
-    __bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
-    if (ADC10MEM < 0x230)
-      P1OUT &= ~BIT6;                       // Clear P1.0 LED off
-     else
-      P1OUT |= BIT6;                        // Set P1.0 LED on
-
+		ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
+    		__bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
+    		if (ADC10MEM < 0x1FF)
+      			P1OUT &= ~BIT6;                       // Clear P1.6 LED off
+     		else
+      			P1OUT |= BIT6;                        // Set P1.6 LED on
 }
 
 // ADC10 interrupt service routine
